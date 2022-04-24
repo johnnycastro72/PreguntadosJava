68990e4c00579091f5cac217affd59afa7febce8
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-24 13:31:18 -0500

    Main
    main method ok

---
+++ src/main/java/console/Preguntados.java
@@ -0,0 +1,11 @@
+package console;
+
+import controller.ControllerGame;
+
+public class Preguntados {
+
+    public static void main(String[] args) {
+        ControllerGame controllerGame = new ControllerGame();
+        controllerGame.GameStart();
+    }
+}
