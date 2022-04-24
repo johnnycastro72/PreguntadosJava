e583654f219e8226e1fc9fa0b8ff8b5350e48c60
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-24 13:22:41 -0500

    Controller
    ControllerRound ok

--- src/main/java/controller/ControllerRound.java
+++ src/main/java/controller/ControllerRound.java
@@ -1,4 +1,14 @@
 package controller;
 
+import domain.Round;
+import repository.RoundRepository;
+
 public class ControllerRound {
+    RoundRepository roundRepo = new RoundRepository();
+
+    @Override
+    public Round obtain(Object o) {
+        Round round = roundRepo.getRound((Integer) o);
+        return round;
+    }
 }
