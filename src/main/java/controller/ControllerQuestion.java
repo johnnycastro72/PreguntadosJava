06e23d7341adb84b031e649857127680bce2512a
Jhonny Castro Clavijo <johnnycastro72@msn.com>
2022-04-24 13:19:33 -0500

    Controllers
    Answer and question controllers created and obtain interface

---
+++ src/main/java/controller/ControllerQuestion.java
@@ -0,0 +1,23 @@
+package controller;
+
+import domain.Category;
+import domain.Question;
+import repository.QuestionRepository;
+
+import java.util.ArrayList;
+
+public class ControllerQuestion implements IQuery{
+    QuestionRepository questionRepo = new QuestionRepository();
+
+    @Override
+    public ArrayList<Question> query(Object o) {
+        ArrayList<Question> questions = questionRepo.getQuestionByCategory((Category) o);
+        return questions;
+    }
+
+    @Override
+    public ArrayList<Question> query() {
+        ArrayList<Question> questions = questionRepo.getQuestions();
+        return questions;
+    }
+}
---
+++ src/main/java/controller/IObtain.java
@@ -0,0 +1,5 @@
+package controller;
+
+public interface IObtain<T> {
+    public T obtain(T o);
+}
