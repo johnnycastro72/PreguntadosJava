38b9bcf5e81211abc12df6381d158abedb1ea03c
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-24 13:21:36 -0500

    Controller
    IQuery ok

--- src/main/java/controller/IQuery.java
+++ src/main/java/controller/IQuery.java
@@ -1,4 +1,8 @@
 package controller;
 
-public interface IQuery {
+import java.util.ArrayList;
+
+public interface IQuery<T> {
+    public ArrayList<T> query(T o);
+    public ArrayList<T> query();
 }
